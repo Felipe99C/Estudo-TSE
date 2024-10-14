586e2cbf9da0b4676180e758d2d5187c0ec9805e
Felipe <felipe.r.cardoso@outlook.com>
2024-10-14 00:07:30 -0300

    Arquivos de ingestao de dados e banco de dados

---
+++ Data/query.sql
@@ -0,0 +1,7 @@
+
+--select * from tb_candidatos
+
+--SELECT * from tb_bens
+
+SELECT* from tb_cassacoes
+
---
+++ src/01_ingestao.py
@@ -0,0 +1,22 @@
+#%%
+import os
+import pandas as pd
+import sqlalchemy
+import json
+
+#%%
+
+# Conexão com o SQLite
+engine = sqlalchemy.create_engine("sqlite:///E:/Estudos/Ciência de dados/Projetos/TSE_2024/Data/database.db")
+
+#copia os dados dos arquivos para a tabela 
+with open("ingestoes.json","r") as open_file:
+    ingestoes = json.load(open_file)
+
+for i in ingestoes:
+    path = i['path']
+    df = pd.read_csv(path, encoding='latin-1', sep=';')
+    # Renomeando as colunas para minúsculas
+   # df.columns = df.columns.str.lower()
+    # Exportando os dados para o banco de dados
+    df.to_sql(i["table"], engine, if_exists="replace", index=False)
