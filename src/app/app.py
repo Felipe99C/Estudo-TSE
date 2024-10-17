368d545b5f6a9060108dcac0e8f8ec53dbae02d9
Felipe <felipe.r.cardoso@outlook.com>
2024-10-17 01:24:44 -0300

    download do db e dos arquivos quando o app é iniciado

--- src/app/app.py
+++ src/app/app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import sqlalchemy
 from utils import make_scatter, make_clusters
 import os
+import gdown
 
 #  insere os caminhos dos diretórios e pastas
 #  para o streamlit conseguir acessar o database mais fácil
@@ -13,26 +14,36 @@ src_path = os.path.dirname(app_path)
 base_path = os.path.dirname(src_path)
 data_path = os.path.join(base_path, "Data")
 
-database_path = os.path.join(data_path,"database.db")
-
-print(database_path)
-#%%
-
-#engine = sqlalchemy.create_engine("sqlite:///E:/Estudos/Ciência de dados/Projetos/TSE_2024/Estudo-TSE/Data/database.db")
+database_path = os.path.join(data_path,"database_gd.db")
 engine = sqlalchemy.create_engine(f"sqlite:///{database_path}")
 
-query_path = os.path.join(app_path, "partidos2.sql")
-with open(query_path, "r") as open_file:
-    query = open_file.read()
+#Link para baixar os arquivos fontes
+@st.cache_data(ttl=60*60*24) #define o tamando do cache para 1 dia (60 segundos * 60 * 24 = 1 dia)
+def download_db():
+    url_database = "https://drive.google.com/uc?id=1lXM0kZqa7AARTW9VS9q0FF9Kcn3N_f5R"
+    gdown.download(url_database, database_path,quiet=False)
 
-df = pd.read_sql(query, engine)
-df.head()
+#deixa o database e as querys em cache por 24h
+@st.cache_data(ttl=60*60*24) #define o tamando do cache para 1 dia (60 segundos * 60 * 24 = 1 dia)
+def create_df():
+    query_path = os.path.join(app_path, "partidos2.sql")
+    with open(query_path, "r") as open_file:
+            query = open_file.read()
+    
+    return pd.read_sql(query, engine)
 
 #%%
 
+download_db()
+
+df = create_df()
 
 welcome = """
 # TSE Analytics - Eleições 2024
+
+Acompanhando a iniciativa Téo me Why em conjunto com a comunidade visando aprender mais sobre análise de dados e ciência de dados.
+
+Como Primeira análise dos partidos, foi focado a questão da diversidade de mulheres e pessoas pretas nas candidaturas de 2024.
 
 """
 
