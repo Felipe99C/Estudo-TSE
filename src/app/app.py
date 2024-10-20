b25f81289a79efae238bd84547ef1a66f256b082
Felipe <felipe.r.cardoso@outlook.com>
2024-10-19 21:35:38 -0300

    finalizando projeto, add novas possibilidades no eixo quanto nas opçoes

--- src/app/app.py
+++ src/app/app.py
@@ -22,7 +22,22 @@ data_path = os.path.join(base_path, "Data")
 def create_df():
     filename = os.path.join(data_path, "data_partidos.parquet")
     return pd.read_parquet(filename)
-    
+
+features_map = {
+    "PERCENTUAL FEMININO": "tx_gen_feminino",
+    "PERCENTUAL RACA PRETA": "tx_cor_raca_preta",
+    "PERCENTUAL RACA PRETA PARDA": "tx_cor_raca_preta_parda",
+    "PERCENTUAL RACA NÃO-BRANCA": "tx_cor_raca_nao_branca",
+    "MEDIA BENS TOTAL":"avgBens",
+    "MEDIA BENS SEM ZEROS (por mil)": "avgBensNotZero",
+    "PERCENTUAL ESTADO CIVIL CASADO(A)": "txEstadoCivilCasado",
+    "PERCENTUAL ESTADO CIVIL SOLTEIRO(A)": "txEstadoCivilSolteiro",
+    "PERCENTUAL ESTADO CIVIL DIVORCIADO(A)": "txEstadoCivilSeparadodivorciado",
+    "MÉDIA IDADE": "avgIdade",
+}
+
+features_options = list(features_map.keys())
+features_options.sort()
 
 #%%
 
@@ -49,22 +64,45 @@ cargos_opt.sort()
 cargos_opt.remove("GERAL")
 cargos_opt = ["GERAL"] + cargos_opt # dessa forma garante que o BR será a primeira opção
 
-col1, col2 = st.columns(2)
 
-print(uf_options)
-
+# Definição dos estados
+col1, col2, = st.columns(2)
 # cria um select box com os estados e passa o valor selecionado para o filtrado mais abaixo
-
-
 with col1:
     estado = st.selectbox(label="Estado", placeholder="Selecione o estado para o filtro", options=uf_options)
-    size = st.checkbox("Tamando das Bolhas")
-    cluster = st.checkbox("Definir Cluster")
 
 with col2:
     cargo = st.selectbox(label="Cargo", placeholder="Selecione o cargo para filtro", options=cargos_opt)
-    st.markdown('')
-    n_cluster = st.number_input("Quantidade de cluster", value=6, format="%d", max_value=10, min_value=1)
+
+# Definição dos eixos
+col1, col2, = st.columns(2)
+with col1:
+    x_option= st.selectbox(label='Eixo x', options=features_options,index = features_options.index("PERCENTUAL FEMININO"))
+    x = features_map[x_option]
+    new_features_options = features_options.copy()
+    new_features_options.remove(x_option)
+
+with col2:
+    #features_options.remove(x_option)
+    default_y_option = "PERCENTUAL RACA PRETA" if "PERCENTUAL RACA PRETA" in new_features_options else new_features_options[0]
+    y_option = st.selectbox(label='Eixo y', options=new_features_options,index=new_features_options.index("PERCENTUAL RACA PRETA"))
+    y = features_map[y_option]
+
+size = st.checkbox("Tamando das Bolhas")
+
+# Definição do uso dos cluster
+col1, col2, = st.columns(2)
+with col1:
+    cluster = st.checkbox("Definir Cluster")
+    if cluster:
+        n_cluster = st.number_input("Quantidade de cluster", value=6, format="%d", max_value=10, min_value=1)
+
+with col2:
+    if cluster:
+        features_options_select = st.multiselect(label='Variaveis para agrupamento',
+                                                 options=features_options,
+                                                 default=["PERCENTUAL FEMININO", "PERCENTUAL RACA PRETA"])
+        features_select = [features_map[i] for i in features_options_select]
 
 # Filtrando os dados pelo estado selecionado
 data = df[(df['SG_UF']==estado) & (df['DS_CARGO']==cargo)].copy()
@@ -73,8 +111,13 @@ total_candidatos = int(data["total_candidaturas"].sum())
 st.markdown(f"Total de Candidaturas: {total_candidatos}")
 
 if cluster:
-    data = make_clusters(data, n_cluster)
+    data = make_clusters(data = data, features=features_select, n=n_cluster)
 
-fig = make_scatter(data, size=size, cluster=cluster)
+fig = make_scatter(data,
+                   x=x , y=y, 
+                   x_label=x_option,
+                   y_label=y_option,
+                   size=size,
+                   cluster=cluster)
 
 st.pyplot(fig)
--- src/app/utils.py
+++ src/app/utils.py
@@ -5,14 +5,15 @@ import seaborn as sn                   # Visualização de dados
 from matplotlib.offsetbox import OffsetImage, AnnotationBbox  # Adicionar imagens e textos em posições customizadas nos gráficos
 from adjustText import adjust_text      # Ajuste automático da posição de textos nos gráficos
 from sklearn import cluster
+from sklearn import preprocessing
 from matplotlib.ticker import PercentFormatter  # Para formatar os eixos em porcentagem
 
-def make_scatter(data, cluster=False, size=False):
+def make_scatter(data, x , y, x_label, y_label, cluster=False, size=False):
 
     config = {
         "data":data,
-        "x":"tx_gen_feminino",
-        "y":"tx_cor_raca_preta",
+        "x":x,
+        "y":y,
         "size":"total_candidaturas", # Tamanho das bolhas representa o total de candidaturas
         "sizes":(5,300),
         "hue":"cluster",
@@ -22,7 +23,7 @@ def make_scatter(data, cluster=False, size=False):
 
     if not cluster:
         del config['hue']
-        #del config['palette']
+        del config['palette']
     
     if not size:
         del config['size']
@@ -43,9 +44,9 @@ def make_scatter(data, cluster=False, size=False):
     texts = []
     for i in data['SG_PARTIDO']:
         data_tmp = data[data['SG_PARTIDO'] == i]
-        x = data_tmp['tx_gen_feminino'].values[0]
-        y = data_tmp['tx_cor_raca_preta'].values[0]
-        texts.append(plt.text(x, y, i, fontsize=9))
+        x_pos = data_tmp[x].values[0]
+        y_pos = data_tmp[y].values[0]
+        texts.append(plt.text(x_pos, y_pos, i, fontsize=9))
 
     adjust_text(texts,
                 force_points=0.0002,
@@ -56,45 +57,50 @@ def make_scatter(data, cluster=False, size=False):
                 )
 
     plt.grid(True)
-    plt.suptitle("Partidos: Cor vs Genero - Eleições 2024")
+    plt.suptitle(f"Partidos: {x_label.title()} VS {y_label.title()} - Eleições 2024")
 
     if size:
         plt.title("Maior a bolha, maior o tamanho do partido", fontdict={"size":9})
 
-    plt.xlabel("Taxa de Mulheres")
-    plt.ylabel("Taxa de Pessoas Pretas")
+    plt.xlabel(x_label.title())
+    plt.ylabel(y_label.title())
 
      # Formatando os eixos em porcentagem
-    ax.xaxis.set_major_formatter(PercentFormatter(1))  # 1 significa que os valores são de 0 a 1
-    ax.yaxis.set_major_formatter(PercentFormatter(1))
+    #ax.xaxis.set_major_formatter(PercentFormatter(1))  # 1 significa que os valores são de 0 a 1
+    #ax.yaxis.set_major_formatter(PercentFormatter(1))
 
+    if x == 'tx_gen_feminino' and y == 'tx_cor_raca_preta':
+        tx_gen_feminino = data["total_gen_feminino"].sum() / data["total_candidaturas"].sum() # Taxa de candidaturas femininas
+        tx_cor_raca_preta = data["total_cor_raca_preta"].sum() / data["total_candidaturas"].sum() # Taxa de pessoas pretas
 
-    tx_gen_feminino = data["total_gen_feminino"].sum() / data["total_candidaturas"].sum() # Taxa de candidaturas femininas
-    tx_cor_raca_preta = data["total_cor_raca_preta"].sum() / data["total_candidaturas"].sum() # Taxa de pessoas pretas
+        # Linhas de referência
 
-    # Linhas de referência
+        plt.hlines(y=tx_cor_raca_preta,
+                   xmin=data["tx_gen_feminino"].min(),
+                   xmax=data["tx_gen_feminino"].max(),
+                   colors='black', alpha=0.6, linestyles='--', label=f"Pessoas Pretas Geral: {100*tx_cor_raca_preta:.0f}%")
 
-    plt.hlines(y=tx_cor_raca_preta,
-               xmin=data["tx_gen_feminino"].min(),
-               xmax=data["tx_gen_feminino"].max(),
-               colors='black', alpha=0.6, linestyles='--', label=f"Pessoas Pretas Geral: {100*tx_cor_raca_preta:.0f}%")
-    
-    plt.vlines(x=tx_gen_feminino,
-               ymin=data["tx_cor_raca_preta"].min(),
-               ymax=data["tx_cor_raca_preta"].max(),
-               colors='tomato', alpha=0.6, linestyles='--', label=f"Mulheres Geral: {100*tx_gen_feminino:.0f}%")
+        plt.vlines(x=tx_gen_feminino,
+                   ymin=data["tx_cor_raca_preta"].min(),
+                   ymax=data["tx_cor_raca_preta"].max(),
+                   colors='tomato', alpha=0.6, linestyles='--', label=f"Mulheres Geral: {100*tx_gen_feminino:.0f}%")
 
-    handles, labels = plt.gca().get_legend_handles_labels()
-    handles = handles[-2:]
-    labels = labels[-2:]
+        handles, labels = plt.gca().get_legend_handles_labels()
+        handles = handles[-2:]
+        labels = labels[-2:]
 
-    # Exibindo a legenda e adicionando o logo
-    plt.legend(handles=handles, labels=labels)
+        # Exibindo a legenda e adicionando o logo
+        plt.legend(handles=handles, labels=labels)
+    else:
+        legend = plt.legend()
+        legend.remove()    
 
     return fig
 
-def make_clusters(data, n=6):
-    model = cluster.KMeans(n_clusters=n, random_state=42)
-    model.fit(data[['tx_gen_feminino','tx_cor_raca_preta']])
+def make_clusters(data, features, n=6):
+    norm = preprocessing.MinMaxScaler()
+    data_norm = norm.fit_transform(data[features])
+    model = cluster.KMeans(n_clusters=n, random_state=42, max_iter=10000)
+    model.fit(data_norm)
     data["cluster"] = model.labels_
     return data
